5f32fb68cdee0a07bac6d8924f93b7b7a7d060ac
taeyun01 <lwe12f675@gmail.com>
2024-07-06 20:25:55 +0900

    feat: supabase ts파일로 재셋팅

---
+++ lib/client/supabase.ts
@@ -0,0 +1,9 @@
+// import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "../../types/supabase";
+
+export const createSupabaseBrowserClient = () =>
+  createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY!
+  );
